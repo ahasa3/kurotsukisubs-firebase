6d053dbf4d70a9d5e5c9d85b0df7939313a66fd3
ahasa3 <ahmadali.sidqi@gmail.com>
2026-03-11 18:49:31 +0700

    change allowed host

---
+++ api/index.py
@@ -0,0 +1,13 @@
+import os
+import sys
+
+sys.path.append(os.path.dirname(os.path.dirname(__file__)))
+
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE",
+    "kurotsukisubs_firebase.settings"
+)
+
+from django.core.wsgi import get_wsgi_application
+
+app = get_wsgi_application()
